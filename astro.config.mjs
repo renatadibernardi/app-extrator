95b954786361a27a37386a6edcf7fd33eea2176a
dibernardi <dibernardi2026@gmail.com>
2026-05-31 08:30:42 -0300

    Fix app extrator local tunnel UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
+import cloudflare from '@astrojs/cloudflare';
+
+const isDevCommand = process.argv.includes('dev');
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://madrinhadosono.online',
   base: '/app-extrator',
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: isDevCommand ? node({ mode: 'standalone' }) : cloudflare(),
 });
